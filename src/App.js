df563f5be49cb44086d576445877b27d00b15b23
LeusMaximus <leus.maximus@gmail.com>
2018-05-10 08:05:08 +0300

    Added header (AppBar) and initial chat-page styles and structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
+import { withStyles } from 'material-ui/styles';
+import CssBaseline from 'material-ui/CssBaseline';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  appFrame: {
+    display: 'flex',
+    height: '100vh',
+  },
+});
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div className={classes.appFrame}>
+        <CssBaseline />
+
+        <AppBar
+          position="absolute"
+        >
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              Chat Name
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
