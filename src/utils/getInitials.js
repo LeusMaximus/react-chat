c95fc9862207bda121946e5739e46ac8c914cfe3
LeusMaximus <leus.maximus@gmail.com>
2018-05-12 10:22:47 +0300

    Fixed formatting in getInitials.js

--- src/utils/getInitials.js
+++ src/utils/getInitials.js
@@ -6,12 +6,12 @@
 export default function(title) {
   try {
     return title
-    .split(' ')
-    .slice(0, 2)
-    .map(word => word[0].toUpperCase())
-    .join('');
+      .split(' ')
+      .slice(0, 2)
+      .map(word => word[0].toUpperCase())
+      .join('');
   } catch (e) {
-    console.error(e);
+      console.error(e);
     return '';
   }
 };
