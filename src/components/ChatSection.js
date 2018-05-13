d56ab423ccea040188e532c510a6eef9f2ad6e43
LeusMaximus <leus.maximus@gmail.com>
2018-05-13 22:57:33 +0300

    Added Go To Bottom button

--- src/components/ChatSection.js
+++ src/components/ChatSection.js
@@ -3,6 +3,12 @@ import React from 'react';
 
 // MUI components
 import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+
+// MUI icons
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+import debounce from 'debounce';
 
 // Own modules
 import MessagesList from './MessagesList';
@@ -13,28 +19,60 @@ const styles = theme => ({
     flexGrow: 1,
     overflowY: 'auto',
   },
+
+  btnDownHolder: {
+    position: 'fixed',
+    right: 30,
+    bottom: 100,
+  },
 });
 
 class ChatSection extends React.Component {
   constructor(props) {
     super(props);
     this.chatAreaRef = React.createRef();
+    this.btnDownHolderRef = React.createRef();
   }
 
   componentDidMount() {
-    this.scrollToLastMessage();
+    const area = this.chatAreaRef.current;
+
+    this.goToLastMessage();
+
+    area.addEventListener('scroll', debounce(this.handleChatAreaScroll.bind(this, area)), 500);
   }
 
   componentDidUpdate() {
-    this.scrollToLastMessage();
+    this.goToLastMessage();
   }
 
-  scrollToLastMessage = () => {
+  goToLastMessage = () => {
     const el = this.chatAreaRef.current;
 
     if (el) {
       el.scrollTop = el.scrollHeight;
     }
+  }
+
+  handleChatAreaScroll = area => {
+    this.setBtnDownStatus(area);
+  }
+
+  // TODO: try change this functionality in future, maybe with state...
+  setBtnDownStatus = area => {
+    const btn = this.btnDownHolderRef.current;
+    const isScrolledToBottom = area.scrollTop > area.scrollHeight - area.offsetHeight - 100;
+
+    if (isScrolledToBottom) {
+      btn.style.visibility = 'hidden';
+    } else {
+      btn.style.visibility = 'visible';
+    }
+  }
+
+  handleGoBottom = e => {
+    e.preventDefault();
+    this.goToLastMessage();
   }
 
   render() {
@@ -43,6 +81,12 @@ class ChatSection extends React.Component {
     return (
       <main className={classes.chatArea} ref={this.chatAreaRef}>
         <MessagesList messages={messages} />
+
+        <div className={classes.btnDownHolder} ref={this.btnDownHolderRef}>
+          <Button variant="fab" color="primary" onClick={this.handleGoBottom}>
+            <ExpandMore />
+          </Button>
+        </div>
       </main>
     );
   }
