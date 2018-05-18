5d88c8340c849af38ad232d179d20467877c10a8
LeusMaximus <leus.maximus@gmail.com>
2018-05-19 01:57:47 +0300

    Added validation to Login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,6 +6,9 @@ import { withStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+// Own modules
+import isTextFieldValid from '../utils/isTextFieldValid';
+
 const styles = theme => ({
   button: {
     marginTop: theme.spacing.unit * 4
@@ -14,20 +17,53 @@ const styles = theme => ({
 
 class LoginForm extends React.Component {
   state = {
-    username: '',
-    password: ''
+    username: {
+      value: '',
+      isValid: true,
+    },
+    password: {
+      value: '',
+      isValid: true
+    },
+  }
+
+  validate = () => {
+    const { username, password } = this.state;
+    const isUserNameValid = isTextFieldValid(username.value);
+    const isPasswordValid = isTextFieldValid(password.value);
+
+    this.setState({
+      username: {
+        ...username,
+        isValid: isUserNameValid,
+      },
+      password: {
+        ...password,
+        isValid: isPasswordValid,
+      },
+    });
+
+    return isUserNameValid && isPasswordValid;
   }
 
   handleChange = event => {
     const { name, value } = event.target;
 
     this.setState(prevState => ({
-      [name]: value
+      [name]: {
+        ...prevState[name],
+        value,
+      }
     }));
   }
 
   handleSubmit = event => {
     event.preventDefault();
+
+    const isValid = this.validate();
+
+    if (!isValid) return;
+
     // TODO: should implement submit functionality
     alert('Soon...')
   }
@@ -41,22 +77,24 @@ class LoginForm extends React.Component {
         <TextField
           label="Username"
           name="username"
-          value={username}
+          value={username.value}
           fullWidth
           required
           margin="normal"
           onChange={this.handleChange}
+          error={!username.isValid}
         />
 
         <TextField
           label="Password"
           name="password"
-          value={password}
+          value={password.value}
           type="password"
           fullWidth
           required
           margin="normal"
           onChange={this.handleChange}
+          error={!password.isValid}
         />
 
         <Button
