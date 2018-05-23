38e2dd17a3a6de863ea0b9cd15db279251a65225
LeusMaximus <leus.maximus@gmail.com>
2018-05-23 16:24:19 +0300

    Added Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'typeface-roboto';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
+
+// For Hot Module Replacement
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(<App />, rootEl);
+  });
+}
 
 registerServiceWorker();
