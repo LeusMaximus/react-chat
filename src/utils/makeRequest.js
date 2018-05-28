3dd35a5224c2eced9a1fd4f5ebbfb2354867b263
LeusMaximus <leus.maximus@gmail.com>
2018-05-28 11:00:15 +0300

    Added makeRequest util function

---
+++ src/utils/makeRequest.js
@@ -0,0 +1,37 @@
+// Vendor modules
+import fetch from 'isomorphic-fetch';
+
+// Own modules
+import config from '../config';
+
+export default function makeRequest({
+  endpoint = '',
+  token = '',
+  body,
+  requestOptions = {}
+}) {
+  const authHeaders = token
+  ? {
+      'Authorization': `Bearer ${token}`,
+    }
+  : {};
+
+  return fetch(`${config.API_URI}${endpoint}`, {
+    method: "GET",
+    headers: {
+      'Accept': 'spplication/json',
+      'Content-Type': 'application/json',
+      ...authHeaders,
+    },
+    body: JSON.stringify(body),
+    ...requestOptions,
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (!data.success) {
+        throw new Error(data.message);
+      }
+
+      return data;
+    });
+}
