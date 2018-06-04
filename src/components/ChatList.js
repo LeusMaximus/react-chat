21803d401ebefde876df049d836fdf1b2360ab46
LeusMaximus <leus.maximus@gmail.com>
2018-06-05 00:40:12 +0300

    Added search chats functionality

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 // MUI components
 import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 // vendor modules
 import nanoid from 'nanoid';
@@ -10,10 +12,27 @@ import nanoid from 'nanoid';
 // Own modules
 import ChatListItem from './ChatListItem';
 
-const ChatList = ({ chats, setActiveChat, activeId }) => (
-  <List component="div">
-    {chats.map(item => <ChatListItem key={nanoid()} item={item} setActiveChat={setActiveChat} activeId={activeId} />)}
-  </List>
-);
+const ChatList = ({ chats, setActiveChat, activeId, searchTerm }) => {
+  const filteredChats = chats
+    .filter(item => item.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+    )
+    .sort((one, two) =>
+        one.title.toLowerCase() <= two.title.toLowerCase() ? -1 : 1
+    );
+
+  return (
+    <List component="div">
+      {
+        filteredChats.length
+          ? filteredChats.map(item => <ChatListItem key={nanoid()} item={item} setActiveChat={setActiveChat} activeId={activeId} />)
+          : <ListItem component="div">
+              <ListItemText primary={'No chats found...'} />
+            </ListItem>
+        }
+    </List>
+  );
+};
 
 export default ChatList;
